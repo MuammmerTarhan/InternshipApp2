5d331b4c043853897abc89f179e34d9edea8f260
Huseyin <huseyinkarakaya006@gmail.com>
2023-08-02 16:16:48 +0300

    02.08.2023

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@
 import React from 'react';
 import Navigator from './src/Navigator';
 import config from './src/confic';
+import CodePush from 'react-native-code-push';
 
 const App: React.FC = () => {
   return <Navigator />;
 };
 
-export default App;
+export default CodePush(App);
